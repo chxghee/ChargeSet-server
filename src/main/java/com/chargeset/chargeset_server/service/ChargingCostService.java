7cda154ac7e8e8ed2a71f6af3a1d2eaed6614d92
chxghee <francis001021@gmail.com>
2025-05-28 10:57:42 +0900

    Feat: 비용 계산 및 충전 스케줄 기능 구현

--- src/main/java/com/chargeset/chargeset_server/service/ChargingCostService.java
+++ src/main/java/com/chargeset/chargeset_server/service/ChargingCostService.java
@@ -12,6 +12,8 @@ import com.chargeset.chargeset_server.repository.evse.EvseRepository;
 import com.chargeset.chargeset_server.repository.reservation.ReservationRepository;
 import com.chargeset.chargeset_server.repository.tou.TouRepository;
 import com.chargeset.chargeset_server.utils.TimeUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,60 +27,35 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChargingCostService {
 
-    private final TouRepository touRepository;
     private final EvseRepository evseRepository;
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
 
-    public NewReservation calcFee(NewReservationRequest request, String idToken, String stationId) {
+    public NewReservation calculateFee(NewReservationRequest request, String idToken, String stationId) {
 
         // 1. 사용자 유효성 검사 - 토큰 아이디 검사
         User user = userRepository.findByIdToken(idToken)
                 .orElseThrow(() -> new IllegalArgumentException("인증된 사용자가 아닙니다!"));
 
-        System.out.println("유저:" + user.getId() + " " + user.getName());
         // 입력받은 시작 시간으로 endTime 계산
         LocalDateTime startTime = request.getStartDate().atTime(request.getStartTime());
         LocalDateTime endTime = startTime.plusMinutes(request.getChargingMinute());
 
         // 2. 해당 시기에 예약 가능한지 -> 해당 시간동안 예약 테이블 확인하여 예약 가능한 evse 찾기
-        List<EvseIdOnly> availableEvseIds = findAvailableEvseIds(stationId, startTime, endTime);
+        List<EvseIdOnly> availableEvseIds = validateChargingRequest(stationId, startTime, endTime);
 
-        if (availableEvseIds.isEmpty()) {
-            throw new IllegalArgumentException("현재 예약 가능한 충전기가 없습니다!");
-        }
-
-        int chargingTime = request.getChargingMinute();
+        int chargingMinute = request.getChargingMinute();
         int chargingWh = request.getTargetEnergyWh();
 
-
-
-        ////계산 편의를 위해 완속은 시간당 30000(분당 500)Wh, 고속은 시간당 60000(분당 1000)Wh 충전으로 변경 (기존 계획: 고속 50000kWh)
-
-        if (chargingTime * 1000 < chargingWh) { // <-  d여기 1000은 어케 나온 숫자인지? chargingTime 분단위 인가요?
+        //  완속은 시간당 6000(분당 100)Wh, 고속은 시간당 60000(분당 1000)Wh 충전
+        if (chargingMinute * 1000 < chargingWh) {
             throw new IllegalArgumentException("요구 충전량을 충전하기 위한 시간이 충분하지 않습니다!");
         }
 
 
-        NewReservation option;
+        ScheduleAndCost scheduleAndCost = calculateScheduleAndCost(chargingMinute, chargingWh, stationId);
 
-
-
-
-        NewChargingProfile chargingProfile = getChargingProfile(startTime);
-
-
-        // 5. 충전 비용 산출 -> TOU 와 ESS 뭐시기로 암튼 비용을 산정해주세요
-        // -> 일단 아직 DB에 저장될 ESS 관련 데이터 스키마를 정하지 않아서 이거 만들떄 편한 형태로 정해주면 그대로 저장하도록 하겠음
-        int cost = getCost();
-        if (stationId.equals("ST-002")) {
-            cost = 410;
-        }
-
-
-        // 일단 이런식으로 이루어 질거 같은데 코드를 짜면서 로직 흐름은 수정해도 됨
-        // 중요한건 마지막 리턴되는 객체에 맞게 데이터를 잘 넣어 주어야 합니다.
-
+        NewChargingProfile chargingProfile = getChargingProfile(scheduleAndCost, startTime);
 
 
         return new NewReservation(
@@ -89,26 +66,49 @@ public class ChargingCostService {
                 TimeUtils.convertDateTimeToUTC(startTime),
                 TimeUtils.convertDateTimeToUTC(endTime),
                 request.getTargetEnergyWh(),
-                cost,
+                scheduleAndCost.getCost(),
                 chargingProfile
-        );        // 마지막으로 생성된 내용을 종합해서 NewReservation 객체를 생성해 주면 됨
+        );
     }
 
-    // 이 부분 구현해 주세요
-    private NewChargingProfile getChargingProfile(LocalDateTime startTime) {
+    private List<EvseIdOnly> validateChargingRequest(String stationId, LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("과거 시각으로는 예약할 수 없습니다!");
+        }
+        List<EvseIdOnly> availableEvseIds = findAvailableEvseIds(stationId, startTime, endTime);
+        if (availableEvseIds.isEmpty()) {
+            throw new IllegalArgumentException("현재 예약 가능한 충전기가 없습니다!");
+        }
+        return availableEvseIds;
+    }
+
+    /**
+     * 스케줄  계산 식
+     * 100 * standardChargingMinute + 1000 * fastChargingMinute = chargingWh
+     * standardChargingMinute + fastChargingMinute = chargingMinute
+     */
+    private ScheduleAndCost calculateScheduleAndCost(int chargingMinute, int chargingWh, String stationId) {
+        StationChargingCost chargingCost = StationChargingCost.from(stationId);
+
+        double fastChargingMinute = (chargingWh - 100.0 * chargingMinute) / 900.0;
+        double standardChargingMinute = chargingMinute - fastChargingMinute;
+        int cost = (int) Math.round(
+                standardChargingMinute * chargingCost.getStandardChargingCostPerMinute() +
+                        fastChargingMinute * chargingCost.getFastChargingCostPerMinute()
+        );
+        return new ScheduleAndCost(cost, standardChargingMinute, fastChargingMinute);
+    }
+
+    private NewChargingProfile getChargingProfile(ScheduleAndCost scheduleAndCost, LocalDateTime startTime) {
         List<ChargingSchedulePeriod> periods = new ArrayList<>();
+        int standardSecond = (int) Math.round(scheduleAndCost.getStandardChargingMinute() * 60);
+        int endSecond = standardSecond + (int) Math.round(scheduleAndCost.getFastChargingMinute() * 60);
+
         periods.add(new ChargingSchedulePeriod(0, 6000, false));
-        periods.add(new ChargingSchedulePeriod(60, 60000, true));
-        periods.add(new ChargingSchedulePeriod(120, 6000, true));
-        periods.add(new ChargingSchedulePeriod(180, 0, true));
+        periods.add(new ChargingSchedulePeriod(standardSecond, 60000, true));
+        periods.add(new ChargingSchedulePeriod(endSecond, 0, false));
 
         return new NewChargingProfile(TimeUtils.convertDateTimeToUTC(startTime), periods);
-    }
-
-    // 이 부분 구현해 주세요
-    private int getCost() {
-        List<Tou> touData = touRepository.findAll();    // TOU 데이터를 가져올 수 있음
-        return 420;
     }
 
     private List<EvseIdOnly> findAvailableEvseIds(String stationId, LocalDateTime startTime, LocalDateTime endTime) {
@@ -123,4 +123,11 @@ public class ChargingCostService {
     }
 
 
+    @Getter
+    @AllArgsConstructor
+    static class ScheduleAndCost {
+        private int cost;
+        private double standardChargingMinute;
+        private double fastChargingMinute;
+    }
 }
